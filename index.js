460710d49328ea0e25a19845ed9b69fde4b66ebe
regasaibrahim <raggasa@gmail.com>
2024-10-02 13:13:28 +0300

    nodekoo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 3000;
+const port = 'https://regasa-student-node.onrender.com/';
 var cors = require('cors')
 
 
